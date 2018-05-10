29ac3c2a5e7b447ca382ba06c815461dd60bf365
hyeonukbhin <bhu@kist.re.kr>
2018-05-10 18:19:06 +0900

    WD Complete

--- weired_dictionary/main.py
+++ weired_dictionary/main.py
@@ -1,19 +1,56 @@
 #!/usr/bin/python3
-#-*- coding: utf-8 -*-
 
-import tools.machine as mc_tool
+import configparser
+
+import weired_dictionary.tools.dictionary as wd_tool
+
+config = configparser.ConfigParser()
+config.read('config.cfg')
+PATH = config['DEFAULT']['PATH']
+FILENAME = config['DEFAULT']['FILENAME']
+INIT_DATA = bool(int(config['DEFAULT']['INIT_DF']))
+
 
 def main():
-    mc_tool.init_stock()
+    if INIT_DATA is True:
+        wd_tool.init_dict()
     while True:
-        mc_tool.show_items()
-        result_order, order = mc_tool.get_order()
-        result_cash, change = mc_tool.get_cash(order)
-        if (result_order == True) and (result_cash == True):
-            mc_tool.pop_items(order)
-            mc_tool.pop_change(change)
-        else:
-            print("잘못된 주문입니다.")
+        print("=============================================")
+        print("      Welcome to Weird Dictionary (WD)!      ")
+        print("1. Show all words in WD")
+        print("2. Add a new word to WD")
+        print("3. Remove some word from WD")
+        print("4. Update some word from WD")
+        print("5. Close WD")
+        print("=============================================")
+        selection = input("Choose what you want to do? : ")
 
-if __name__ == "__main__":
-    main()
+        if selection == '1':
+            wd_tool.show_dict()
+        elif selection == '2':
+            key = input("Fill in a item you want to add? : ")
+            value = input("Fill in a description of the item you want to add? :")
+            wd_tool.add_dict(key, value)
+        elif selection == '3':
+            wd_tool.show_dict()
+            key = input("Fill in the item you want to remove? : ")
+            print(wd_tool.check_index(key))
+            if wd_tool.check_index(key) is True:
+                wd_tool.remove_dict(key)
+            else:
+                print("There are information : {} in the dictionary.".format(key))
+        elif selection == '4':
+            key = input("Fill in the item you want to update? : ")
+            if wd_tool.check_index(key) is True:
+                wd_tool.check_index(key, value)
+                value = input("Fill in a description of the item you want to add? :")
+            else:
+                print("There are information : {} in the dictionary.".format(key))
+        else:
+            print("\nGood Bye.")
+            break
+
+        input("Press Enter to continue...")
+
+
+main()
--- weired_dictionary/tools/dataframe.py
+++ weired_dictionary/tools/dataframe.py
@@ -1,23 +1,22 @@
 #!/usr/bin/python3
-#-*- coding: utf-8 -*-
 
-import numpy as np
 import pandas as pd
-
-
+import configparser
+import os
+config = configparser.ConfigParser()
+config.read('config.cfg')
+FILEPATH = config['DEFAULT']['FILEPATH']
 
 def save_df(df, filename):
     df.to_csv(filename, sep=',')
-
 
 def read_df(filename):
     df = pd.read_csv(filename, sep=',', na_values=".", index_col=0)
     return df
 
-
 def update_df(index, columns, post_value, df=None):
     if df is None:
-        df = read_df('Data.csv')
+        df = read_df(FILEPATH)
     df = df.copy()
 
     pre_value = df.loc[index, columns]
@@ -27,20 +26,27 @@ def update_df(index, columns, post_value, df=None):
 
 def add_df(index, columns, init_value, df=None):
     if df is None:
-        df = read_df('Data.csv')
+        df = read_df(FILEPATH)
     df.loc[index, columns] = init_value
     print("{} : {} of {} is added.".format(columns, init_value, index))
     return df
 
 def remove_row_df(index, df=None):
     if df is None:
-        df = read_df('Data.csv')
-    df.drop([index])
+        df = read_df(FILEPATH)
+    df = df.drop([index])
     print("row : {} is removed.".format(index))
     return df
 
 def check_df(index, columns, df=None):
     if df is None:
-        df = read_df('Data.csv')
+        df = read_df(FILEPATH)
     result = df[columns][index]
     return result
+
+def get_indexlist(df=None):
+    if df is None:
+        df = read_df(FILEPATH)
+    result = df.index.tolist()
+    return result
+
